fda5639a93208a20a3fd0867d95f2016c31a175c
11bowale188 <11bowale188@BA-1432-DT-02.bville.org>
2015-04-24 10:26:48 -0400

    Added a sentence for my dating profile
    buzzzzzzzzzzz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print "Let's print stuff!"
 print "Say your name, and something cool about yourself!"
-
+print " I am Alex Bowen and I like long walks on the beach and puppies and stuff"
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
 
