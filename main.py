f33901d7f799f691b7c98a2036c1585219512070
1egoman <rsg1egoman@gmail.com>
2015-04-24 16:15:06 -0400

    move all messages below the comment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 print "Let's print stuff!"
+
+# each person should put a print statement for themselves
+# for example: print "Hi, I'm John, and I like pie."
+print "JUBLAAZE - Ryan^3"
 print "Halfbreeds love bicycles! -Jarrod"
 print "My name is Michael Joseph King Zucco, and I like to eat Cajun Chicken Pasta"
 print "I am Alex Bowen and I like long walks on the beach and puppies and stuff"
@@ -10,7 +14,3 @@ print "SpeHou's Crusaders will rise, is there SpeHou in New Zealand??"
 print "SpeHou likes ice cream and unicorns"
 print "PYB"
 print "hello world!"
-
-# each person should put a print statement for themselves
-# for example: print "Hi, I'm John, and I like pie."
-print "JUBLAAZE - Ryan^3"
