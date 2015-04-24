6ae3a673eb7be8df19f1c33e53b29a62c5a92525
sahoude <houdespen@mylcsd.org>
2015-04-24 10:12:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print "Let's print stuff!"
 print "Say your name, and something cool about yourself!"
+print "SpeHou likes ice cream and unicorns"
 
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
