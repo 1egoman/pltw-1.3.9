de60ff7c058488956b421584b922b782b0b51c35
siko9iae <12cumjar654@BA-1432-DT-11.bville.org>
2015-04-24 10:26:38 -0400

    Added a print statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print "Let's print stuff!"
 print "Say your name, and something cool about yourself!"
-
+print "Halfbreeds love bicycles! -Jarrod"
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
 
