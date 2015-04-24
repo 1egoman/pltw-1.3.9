c888af775dd813ced499cb1544f0191553ae50fb
12ZucMic781 <12ZucMic781@BA-1432-DT-22.bville.org>
2015-04-24 10:28:14 -0400

    YO brodda from anotha mother
    How are you doing? I just ate a bowl of rice, and it tasted delicioso.
    Dora the Explora loves to eat rice. Dora's favorite rice is chicken
    fried rice.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print "Let's print stuff!"
-print "Say your name, and something cool about yourself!"
+print "My name is Michael Joseph King Zucco, and I like to eat Cajun Chicken Pasta"
 
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
